995ed2cb58c96580270b1519defaec2018b7465e
Zerayasl <Zerayasl@students.zhaw.ch>
2026-04-10 05:41:38 +0200

    LNW06

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/ToDoTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/ToDoTest.java
@@ -1,6 +1,8 @@
 package ch.zhaw.iwi.devops.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +14,19 @@ public class ToDoTest {
         assertEquals("title", toDo1.getTitle());
         assertEquals("description", toDo1.getDescription());
         assertEquals(1, toDo1.getId());
+    }
+
+    @Test
+    void testToDoNotNull() {
+        var toDo = new ToDo(1, "titel", "beschreibung");
+        assertNotNull(toDo);
+    }
+
+    @Test
+    void testToDoDifferentIds() {
+        var toDo1 = new ToDo(1, "a", "b");
+        var toDo2 = new ToDo(2, "a", "b");
+        assertNotEquals(toDo1.getId(), toDo2.getId());
     }
     
 }
